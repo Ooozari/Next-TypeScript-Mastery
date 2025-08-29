5da5d25891fb84803b15b5bba3e35871b9114852
Ooozari <uzairasiff1227@gmail.com>
2025-08-29 15:58:15 +0500

    feat(db): add MongoDB connection utility with Mongoose

---
+++ truevoice/src/lib/dbConnect.ts
@@ -0,0 +1,29 @@
+import mongoose, { mongo } from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number,
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+
+    // case one is connection is already establish
+    if (connection.isConnected) {
+        console.log("Already connected to database")
+        return;
+    }
+
+    try {
+        // consolelog it so see details
+        const db = await mongoose.connect(process.env.MONGODB_URL || '', {})
+
+        connection.isConnected = db.connections[0].readyState
+        console.log("Database connected successfully")
+    } catch (error) {
+        console.log("Database connected failed", error)
+        process.exit(1)
+    }
+}
+
+export default dbConnect

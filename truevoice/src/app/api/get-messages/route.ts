522999e1c8f74c98d7348febc1413fca9ea3843f
Ooozari <uzairasiff1227@gmail.com>
2025-09-07 01:27:14 +0500

    fix(api): correct $sort stage field syntax in get-messages endpoint

--- truevoice/src/app/api/get-messages/route.ts
+++ truevoice/src/app/api/get-messages/route.ts
@@ -32,7 +32,7 @@ export async function GET(request: Request) {
 
             { $match: { _id: userId } },
             { $unwind: '$messages' },
-            { $sort: { '$messages.createdAt': -1 } },
+            { $sort: { 'messages.createdAt': -1 } },
             { $group: { _id: '$_id', messages: { $push: '$messages' } } }
         ])
 

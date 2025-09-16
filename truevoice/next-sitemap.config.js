91cb987ff8e5038f6125fb1fe8f7c011a836b583
Ooozari <uzairasiff1227@gmail.com>
2025-09-16 22:25:45 +0500

    chore(seo): add next-sitemap config for generating sitemap.xml and robots.txt

---
+++ truevoice/next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://truevoicehub.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  // changefreq: "daily",
+  // priority: 0.7,
+};
+
+module.exports = config;
